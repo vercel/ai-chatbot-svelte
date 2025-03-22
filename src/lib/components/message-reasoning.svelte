149416af43b169632a4186a78cf5cbbbf4c1e62f
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2025-03-21 19:52:43 -0600

    fix: Fix reasoning collapse jank

--- src/lib/components/message-reasoning.svelte
+++ src/lib/components/message-reasoning.svelte
@@ -2,8 +2,23 @@
 	import LoaderIcon from './icons/loader.svelte';
 	import ChevronDownIcon from './icons/chevron-down.svelte';
 	import { Markdown } from './markdown';
+	import { slide } from 'svelte/transition';
+	import { cubicInOut } from 'svelte/easing';
+	import { getLock } from '$lib/hooks/lock';
+	import { tick } from 'svelte';
 	let { loading, reasoning }: { loading: boolean; reasoning: string } = $props();
 	let expanded = $state(false);
+	const scrollLock = getLock('messages-scroll');
+
+	function lockScrolling() {
+		scrollLock.locked = true;
+	}
+
+	function unlockScrolling() {
+		tick().then(() => {
+			scrollLock.locked = false;
+		});
+	}
 </script>
 
 <div class="flex flex-col">
@@ -30,5 +45,16 @@
 		</div>
 	{/if}
 
-	<Markdown md={reasoning} />
+	{#if expanded}
+		<div
+			transition:slide={{ duration: 200, easing: cubicInOut }}
+			onintrostart={lockScrolling}
+			onintroend={unlockScrolling}
+			onoutrostart={lockScrolling}
+			onoutroend={unlockScrolling}
+			class="mb-2 mt-4 flex flex-col gap-4 border-l pl-4 text-zinc-600 dark:text-zinc-400"
+		>
+			<Markdown md={reasoning} />
+		</div>
+	{/if}
 </div>
